2ec919bb76940802021191967a808d03faa22c11
Minga992 <minga_titere@hotmail.com>
2016-04-03 12:31:53 -0300

    ej 2 hecho, esta la explicita de 3, porque la funcion posta no me esta resultando tan trivial

--- TP1/Tp.hs
+++ TP1/Tp.hs
@@ -20,6 +20,8 @@ mean :: [Float] -> Float
 mean xs = realToFrac (sum xs) / genericLength xs
 
 split :: Eq a => a -> [a] -> [[a]]
+
+--Verision explicita:
 --split d [] = []
 --split d xs = takeWhile (\= d) xs : (split d tail(dropWhile (\= d) xs))
 
@@ -27,14 +29,25 @@ split :: Eq a => a -> [a] -> [[a]]
 --split d (x:xs) = if d == x then []:split(xs)
 --       				   	else (x:head(split(xs))):tail(split(xs))
 
+--Version final
 split d = foldr (\x (xs:xss) -> if x == d then []:xs:xss else (x:xs):xss) [[]]
 -- si me encuentro con un delimitador, es una palabra aparte.  Si no, la letra es parte de la misma palabra.
 
 
 longitudPromedioPalabras :: Extractor
-longitudPromedioPalabras = undefined
+longitudPromedioPalabras xs = mean map genericLength split " " xs
+-- Sin los parentesis se ve horrible, pero la idea es, hago el split sacando los espacios del medio, eso me da una lista de listas,
+-- le aplico map con genericLength, que dada una lista te devuelve su longitud en algun formato(supongo que es adaptable o algo asi,
+-- o sino no entendi) y a eso le aplico mean que es una suerte de promedio.
+
 
 cuentas :: Eq a => [a] -> [(Int, a)]
+
+--Version explicita:
+--cuentas [] 	 = []
+--cuentas (x:xs) = (genericLength filter (= x) x:xs) : cuentas filter (\= x) x:xs
+
+--se podria haber hecho con head, pero me parecio mas claro asi
 cuentas = undefined
 
 repeticionesPromedio :: Extractor
