9067dc3aee99661edaddbd7a20c8e92b6f794234
fedesuarez <elgeniofederico@gmail.com>
2016-05-30 01:26:52 -0300

    Primeros dos ejercicios del TP

--- TP2/esqueleto-diccionarios/esqueleto.pl
+++ TP2/esqueleto-diccionarios/esqueleto.pl
@@ -26,3 +26,11 @@ ej(1, [rombo, cuadrado, espacio, perro, cuadrado, sol, cuadrado]).
 ej(2, [rombo, cuadrado, espacio, perro, triangulo, sol, cuadrado]).
 
 ej(3, [rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, gato]).
+
+diccionario_lista(S):-diccionario(X), string_codes(X,S).
+
+juntar_con([X],_,X).
+juntar_con([X|L],J,R1):-juntar_con(L,J,R2),append(X,[J],Y),append(Y,R2,R1).
+
+% palabras así todavìa no anda
+palabras(S,[X|P]):-append(X,[espacio | Y],S), palabras(Y,P).
