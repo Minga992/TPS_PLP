c2318125a4d20fcd973bbd947f99bbbbe0024d72
Alejandro Ruben Mignanelli <amignanelli@ws1.labo3>
2016-06-27 21:14:44 -0300

    tests algunos

--- TPTlen/prueba_parser.py
+++ TPTlen/prueba_parser.py
@@ -9,13 +9,32 @@ parser = yacc(module=parser_rules)
 
 #text = "inicio = \"Hola\";\ni =0;\ndo{ i++;\ninicio += \" \";\n}while (j % 32 != 0); print (\"Mundo!\");"
 #text = "inicio = \"Hola\";\nj = length(inicio);"
-text = "x = (1<2)?\"si\":\"no\";"
+#text = "x = (1<2)?\"si\":\"no\";"
 #text += "x = capitalizar(inicio);"
 #text += "i =0;\n"
 #text += "do{ i++;\n"
 #text += "inicio += \" \"; \n"
 #text += "j = length(inicio)*i;\n"
 #text += "}while (j % 32 != 0); print (\"Mundo!\");"
+
+#probando parentesis
+#text = "(((parentesis))) = (((5)*4)+(((6*6)))) + 8;"
+
+#if con cosas turbias
+#text = "if (\"\" == \"\" ) a = 3 + 4; else b = 4+3; a= b + 5 ;"
+#text = "a=3; a = a + 3;"
+
+#hago faltar cosas, cambio cosas en el medio, y los tipos lo soportan
+#text = "a = true; b=5; for (;a;b++) if( a ) {b=a; if (b) b=a;} else c = 8;"
+#text = " a= true ;b=a; if (b) b=a;"
+#text = "a = true; b=5; for (;a;++b) b=b;"
+#text = "a=1; a++;for (;true;) a=5;"
+#text = "a=1; a++;"
+
+#con esto probemos errores
+#text = "a=3; a++; a--; a += 6; a -= 5; a *= 3; print (length( \"pelado\")); b= (a==8); if (b) a= a; c = a==8? a:a;"
+#text = "a = 3;b= (a==8); if (b) a= a; c = a==8? a:a;"
+#text = "a = 3==3.4"
 
 ast = parser.parse(text, lexer)
 
